2cbdc3f6eebc04ab665102aa528c91e97f4dc127
Triston Lincoln <79023422+trl6895@users.noreply.github.com>
2024-09-04 10:37:25 -0400

    Completed Mathematical surfaces tutorials

--- Sine Wave/Assets/Scripts/Graph.cs
+++ Sine Wave/Assets/Scripts/Graph.cs
@@ -16,20 +16,11 @@ public class Graph : MonoBehaviour
     void Awake()
     {
         float step = 2f / resolution;
-        Vector3 position = Vector3.zero;
         var scale = Vector3.one * step;
         points = new Transform[resolution * resolution];
-        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
+        for (int i = 0; i < points.Length; i++)
         {
-            if (x == resolution)
-            {
-                x = 0;
-                z++;
-            }
             Transform point = points[i] = Instantiate(pointPrefab);
-            position.x = ((x + 0.5f) * step) - 1f;
-            position.z = ((z + 0.5f) * step) - 1f;
-            point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
         }
@@ -39,12 +30,18 @@ public class Graph : MonoBehaviour
     {
         FunctionLibrary.Function f = FunctionLibrary.GetFunction(function);
         float time = Time.time;
-        for (int i = 0; i < points.Length; i++)
+        float step = 2f / resolution;
+        float v = (0.5f * step) - 1f;
+        for (int i = 0, x = 0, z = 0; i < points.Length; i++, x++)
         {
-            Transform point = points[i];
-            Vector3 position = point.localPosition;
-            position.y = f(position.x, position.z, time);
-            point.localPosition = position;
+            if (x == resolution)
+            {
+                x = 0;
+                z++;
+                v = ((z + 0.5f) * step) - 1f;
+            }
+            float u = ((x + 0.5f) * step) - 1f;
+            points[i].localPosition = f(u, v, time);
         }
     }
 }
